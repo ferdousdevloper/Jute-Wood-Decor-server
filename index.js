d5ebf3f35f3d31ac3fc010fee33dc384bf11c82c
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-04-27 19:05:23 +0600

    delete section created

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
         res.send(result);
     })
 
+    app.delete('/decor/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query={_id: new ObjectId(id)}
+      const result = await decorCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.get('/myCraftList/:email', async(req, res)=>{
         console.log(req.params.email);
         const result = await decorCollection.find({email:req.params.email}).toArray();
