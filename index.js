ca23f357be9edd5f35aabfc8cb5c050633e6626f
Your Nameferdousdevloper <eng.mfhossain@gmail.com>
2024-04-29 12:18:40 +0600

    some code removed

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.use(cors({
 app.use(express.json());
 
 
-//juteWoodDecor
-//pass: inPINFkvKLOTu3sZ
-
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dizfzlf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
